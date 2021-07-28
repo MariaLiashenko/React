6041976725d0761f9a96ef3094e6cb71b65c7d7d
Maria Lyashenko <mariia.liashenko@nure.ua>
2021-07-28 15:09:48 +0300

    change information about peopole

--- component/src/index.js
+++ component/src/index.js
@@ -5,12 +5,39 @@ import CommentDetail from './CommentDetail';
 
 const App = () =>{
     return (
+        
         <div className = "ui container comments">
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
+            
+            <CommentDetail 
+                author ="Sam"
+                timeAgo = "Today 5 pm" 
+                text = "beautifull"
+                avatar = {faker.image.avatar()}
+            />
+            <CommentDetail 
+                author ="Dup" 
+                timeAgo = "Today 7 pm" 
+                text = "nice"
+                avatar = {faker.image.avatar()}
+            />
+            <CommentDetail 
+                author ="Swag" 
+                timeAgo = "Today 1 am" 
+                text = "cool"
+                avatar = {faker.image.avatar()}
+            />
+            <CommentDetail 
+                author ="Same" 
+                timeAgo = "Today 10 am" 
+                text = "beautifull))))"
+                avatar = {faker.image.avatar()}
+            />
+            <CommentDetail 
+                author ="Samuel" 
+                timeAgo = "Today 9 pm" 
+                text = "1000/10"
+                avatar = {faker.image.avatar()}
+            />
 
         </div>
     );
