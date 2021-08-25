b23ebd9d349742f2d630722f8c5afcd9481c3547
Maria Lyashenko <mariia.liashenko@nure.ua>
2021-08-25 20:13:58 +0300

    seasons css + loading

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,6 +1,37 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
+const SeasonConfig = {
+    summer:{
+        text : "Как же задолбала жара +35",
+        iconName : "sun"
+    },
+    winter:{
+        text : "Холодно, хочуууу летооо ",
+        iconName : "snowflake"
+    }
+};
+
+const getSeason = (lat,month) => {
+    if(month > 2 && month < 9)  {
+        return lat > 0 ? "summer" : "winter";
+    } else {
+        return lat > 0 ? "winter" : "summer";
+    }
+}
+
 const SeasonDisplay = props => {
-    return 
+    const season = getSeason(props.lat, new Date().getMonth());
+    // const text = season === "winter" ? "Холодно" : "Жарко";
+    // const icon = season === "winter" ? "snowflake": "sun";
+    const {text, iconName} = SeasonConfig[season]; 
+    return (
+    <div className = {`season-display ${season}`}>
+        <i className = {`icon-left massive ${iconName} icon`}></i>
+         <h1>{text}</h1>
+        <i className = {`icon-right massive ${iconName} icon`}></i>
+    </div>
+    );
     
 };
+export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeaonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     
@@ -25,7 +26,7 @@ class App extends React.Component {
             return <SeaonDisplay lat = {this.state.lat}/>;
         }
      
-      return <div>Loading</div>
+      return <Spinner message = "Please accept location request"/>
     
     }
     
