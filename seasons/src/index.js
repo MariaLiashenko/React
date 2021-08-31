8c17647183e62f0d5333ac14a0934f1bd6399c7c
Maria Lyashenko <mariia.liashenko@nure.ua>
2021-08-31 13:49:44 +0300

    last commit

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import SeaonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
@@ -18,7 +18,7 @@ class App extends React.Component {
     );
       
     }
-    render() {
+    renderContent() {
         if(this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>;
         }
@@ -29,8 +29,15 @@ class App extends React.Component {
       return <Spinner message = "Please accept location request"/>
     
     }
-    
+        render() {
+        return(
+            <div className = "border red">
+                {this.renderContent()}
+            </div>
+        );
+    }
 };
+
 
 ReactDOM.render(
     <App />,
